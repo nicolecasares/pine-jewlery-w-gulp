a45b81245579920f8582ab42bc0e999dcfe3361d
nicolecasares <nicolecasares@gmail.com>
2018-01-30 15:11:12 -0800

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+})
+
+gulp.task('sass', function(){
+  return gulp.src('app/scss/styles.scss')
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(gulp.dest('app/css'))
+});
+
+gulp.task('watch', function(){
+  gulp.watch('app/scss/**/*.scss', ['sass']); 
+  // Other watchers
+})
+
+gulp.task('sass', function() {
+  return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('watch', ['browserSync'], function (){
+  gulp.watch('app/scss/**/*.scss', ['sass']); 
+  // Other watchers
+})
+
+gulp.task('watch', ['browserSync', 'sass'], function (){
+  gulp.watch('app/scss/**/*.scss', ['sass']); 
+  // Other watchers
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
